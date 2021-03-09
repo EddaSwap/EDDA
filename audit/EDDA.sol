1e1b1c5088ab9a7a8a62974a375ac5e041c10514
viktor <viktor@axonian.com>
2021-03-09 17:03:03 +0200

    Refactored brand rename

--- audit/EDDA.sol
+++ audit/EDDA.sol
@@ -601,7 +601,7 @@ contract Governable is Context {
 }
 
 contract EDDA is ERC20, ERC20Detailed, Governable {
-    constructor () public ERC20Detailed("Yggdrasil", "EDDA", 18) {
+    constructor () public ERC20Detailed("EDDA", "EDDA", 18) {
         // Mint total supply to Governance during contract creation.
         // _mint is internal funciton of Openzeppelin ERC20 contract used to create all supply.
         // After contract creation, there is no way to call _mint() function on deployed contract.
